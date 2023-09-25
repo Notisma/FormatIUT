c8de8f954aa354a2756ff0810ea0d56f3582b866
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-09-25 21:44:41 +0200

    Commit Initial.

---
+++ web/vueGenerale.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styleIndex.css">
+    <title>Format'IUT</title>
+    <link rel="icon" type="image/png" href="Data/UM.png"/>
+</head>
+<body>
+
+</body>
+</html>
