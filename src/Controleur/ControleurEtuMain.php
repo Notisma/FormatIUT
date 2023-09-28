67ab1c72aaa5df3a968decce690a71392fb680dc
Romain Touzé <romain.touze@etu.umontpellier.fr>
2023-09-28 13:24:27 +0200

    Ajout d'un début de menu dans accueilEtu

--- src/Controleur/ControleurEtuMain.php
+++ src/Controleur/ControleurEtuMain.php
@@ -5,12 +5,15 @@ namespace App\FormatIUT\Controleur;
 class ControleurEtuMain extends ControleurMain
 {
     public static function afficherAccueilEtu(){
-        self::afficherVue("vueGenerale.php",["menu"=>self::getMenu(),"chemin"=>"vueAccueilEtudiant.php"]);
+        self::afficherVue("vueGenerale.php",["menu"=>self::getMenu(),"chemin"=>"vueAccueilEtudiant.php","titrePage"=>"Accueil"]);
     }
 
     public static function getMenu(): array
     {
-        return array();
+        return array(
+            array("image"=>"../ressources/images/accueil.png","label"=>"Accueil","lien"=>"?action=afficherAccueilEtu&controleur=EtuMain"),
+            array("image"=>"../ressources/images/se-deconnecter.png","label"=>"Se déconnecter","lien"=>"controleurFrontal.php")
+        );
     }
 
 }
--- src/vue/vueAccueilEtudiant.php
+++ src/vue/vueAccueilEtudiant.php
@@ -1,6 +1,5 @@
 <html>
     <body>
-        <h1>Bienvenue sur Format'IUT</h1>
 
     </body>
 </html>
