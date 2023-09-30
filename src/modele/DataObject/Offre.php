a402a1aec1164f5ec180b4ea19ea6964d1ddfcc4
Tordeux Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-09-30 13:53:37 +0200

    Offre

---
+++ src/modele/DataObject/Offre.php
@@ -0,0 +1,141 @@
+<?php
+namespace App\FormatIUT\modele\DataObject;
+
+use Cassandra\Date;
+
+class Offre{
+    private int $idOffre;
+    private string $nomOffre;
+    private Date $dateDebut;
+    private Date $dateFin;
+    private string $sujet;
+    private string $detailProjet;
+    private float $gratification;
+    private int $dureeHeures;
+    private int $joursParSemaine;
+    private int $nbHeuresHebdo;
+
+
+    public function getIdOffre(): int
+    {
+        return $this->idOffre;
+    }
+
+    public function setIdOffre(int $idOffre): void
+    {
+        $this->idOffre = $idOffre;
+    }
+
+    public function getNomOffre(): string
+    {
+        return $this->nomOffre;
+    }
+
+    public function setNomOffre(string $nomOffre): void
+    {
+        $this->nomOffre = $nomOffre;
+    }
+
+    public function getDateDebut(): Date
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(Date $dateDebut): void
+    {
+        $this->dateDebut = $dateDebut;
+    }
+
+    public function getDateFin(): Date
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(Date $dateFin): void
+    {
+        $this->dateFin = $dateFin;
+    }
+
+    public function getSujet(): string
+    {
+        return $this->sujet;
+    }
+
+    public function setSujet(string $sujet): void
+    {
+        $this->sujet = $sujet;
+    }
+
+    public function getDetailProjet(): string
+    {
+        return $this->detailProjet;
+    }
+
+    public function setDetailProjet(string $detailProjet): void
+    {
+        $this->detailProjet = $detailProjet;
+    }
+
+    public function getGratification(): float
+    {
+        return $this->gratification;
+    }
+
+    public function setGratification(float $gratification): void
+    {
+        $this->gratification = $gratification;
+    }
+
+    public function getDureeHeures(): int
+    {
+        return $this->dureeHeures;
+    }
+
+    public function setDureeHeures(int $dureeHeures): void
+    {
+        $this->dureeHeures = $dureeHeures;
+    }
+
+    public function getJoursParSemaine(): int
+    {
+        return $this->joursParSemaine;
+    }
+
+    public function setJoursParSemaine(int $joursParSemaine): void
+    {
+        $this->joursParSemaine = $joursParSemaine;
+    }
+
+    public function getNbHeuresHebdo(): int
+    {
+        return $this->nbHeuresHebdo;
+    }
+
+    public function setNbHeuresHebdo(int $nbHeuresHebdo): void
+    {
+        $this->nbHeuresHebdo = $nbHeuresHebdo;
+    }
+
+    public function __construct(int $idOffre, string $nomOffre, Date $dateDebut, Date $dateFin, string $sujet, string $detailProjet, float $gratification, int $dureeHeures, int $joursParSemaine, int $nbHeuresHebdo)
+    {
+        $this->idOffre = $idOffre;
+        $this->nomOffre = $nomOffre;
+        $this->dateDebut = $dateDebut;
+        $this->dateFin = $dateFin;
+        $this->sujet = $sujet;
+        $this->detailProjet = $detailProjet;
+        $this->gratification = $gratification;
+        $this->dureeHeures = $dureeHeures;
+        $this->joursParSemaine = $joursParSemaine;
+        $this->nbHeuresHebdo = $nbHeuresHebdo;
+    }
+
+    public static function construireDepuisTableau(array $offreFormatTableau) : Offre
+    {
+        return new Offre($offreFormatTableau[0], $offreFormatTableau[1], $offreFormatTableau[2], $offreFormatTableau[3]
+            , $offreFormatTableau[4], $offreFormatTableau[5], $offreFormatTableau[6], $offreFormatTableau[7]
+            , $offreFormatTableau[8], $offreFormatTableau[9]);
+    }
+}
+
+?>
