e568476fb120e297b927d869b96e397ec35763af
Romain Touzé <romain.touze@etu.umontpellier.fr>
2023-10-03 22:00:09 +0200

    vueMesOffres.php pour différencier les stages des alternances ou les deux(Offre)

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -11,9 +11,9 @@ namespace App\FormatIUT\Configuration;
 class Configuration{
     static private array $config= array(
      'hostname'=>'localhost',
-     'database'=>'touzerSAE',
-     'login'=>'root',
-     'password'=>'root',
+     'database'=>'loyet',
+     'login'=>'loyet',
+     'password'=>'26032004',
      'port' =>'3306'
     );
 
