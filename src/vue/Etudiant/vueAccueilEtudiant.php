4cac5a9dd91aa1a7cc4bad67c346a51f3bd7e472
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-10-03 11:40:15 +0200

    Ajout des annonces en html (version 1)

--- src/vue/Etudiant/vueAccueilEtudiant.php
+++ src/vue/Etudiant/vueAccueilEtudiant.php
@@ -22,54 +22,73 @@
 
     <div class="nouveautesWrap">
         <div class="nouveautesStages">
-            <h4>Nouveautés Stages :</h4>
+            <h4>Nouveautés Stages de la semaine :</h4>
             <div class="conteneurAnnonces">
                 <a href="https://google.fr">
                     <div class="imagesAnnonce">
-
+                        <img src="../ressources/images/haut-parleur.png" alt="image de l'annonce">
                     </div>
                     <div class="texteAnnonce">
-                        <h4>Stage à IKEA</h4>
-                    </div>
-                </a>
-                <a href="https://google.fr">
-                    <div class="imagesAnnonce">
-
-                    </div>
-                    <div class="texteAnnonce">
-                        <h4>Stage à IKEA</h4>
-                    </div>
-                </a>
-                <a href="https://google.fr">
-                    <div class="imagesAnnonce">
-
-                    </div>
-                    <div class="texteAnnonce">
-                        <h4>Stage à IKEA</h4>
-                    </div>
-                </a>
-                <a href="https://google.fr">
-                    <div class="imagesAnnonce">
-
-                    </div>
-                    <div class="texteAnnonce">
-                        <h4>Stage à IKEA</h4>
-                    </div>
-                </a>
-                <a href="https://google.fr">
-                    <div class="imagesAnnonce">
-
-                    </div>
-                    <div class="texteAnnonce">
-                        <h4>Stage à IKEA</h4>
+                        <h4>CREDIT AGRICOLE TECH</h4>
+                        <div class="detailsAnnonce">
+                            <div class="lieuRemun">
+                                <div class="lieuAnnonce">
+                                    <img src="../ressources/images/emplacement.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">Montpellier, 34</p>
+                                </div>
+                                <div class="remunAnnonce">
+                                    <img src="../ressources/images/euros.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">280/m</p>
+                                </div>
+                            </div>
+                            <div class="dureeLibelle">
+                                <div class="dureeAnnonce">
+                                    <img src="../ressources/images/histoire.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">3 mois</p>
+                                </div>
+                                <div class="libelleAnnonce">
+                                    <img src="../ressources/images/emploi.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">Développeur Front-end</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </a>
             </div>
         </div>
         <div class="nouveautesAltern">
-            <h4>Nouveautés Alternances :</h4>
-            <div id="annonceAltern">
-
+            <h4>Nouveautés Alternances de la semaine :</h4>
+            <div class="conteneurAnnonces">
+                <a href="https://google.fr">
+                    <div class="imagesAnnonce">
+                        <img src="../ressources/images/haut-parleur.png" alt="image de l'annonce">
+                    </div>
+                    <div class="texteAnnonce">
+                        <h4>DELL FRANCE</h4>
+                        <div class="detailsAnnonce">
+                            <div class="lieuRemun">
+                                <div class="lieuAnnonce">
+                                    <img src="../ressources/images/emplacement.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">Montpellier, 34</p>
+                                </div>
+                                <div class="remunAnnonce">
+                                    <img src="../ressources/images/euros.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">295/m</p>
+                                </div>
+                            </div>
+                            <div class="dureeLibelle">
+                                <div class="dureeAnnonce">
+                                    <img src="../ressources/images/histoire.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">6 mois</p>
+                                </div>
+                                <div class="libelleAnnonce">
+                                    <img src="../ressources/images/emploi.png" alt="image" class="imagesPuces">
+                                    <p class="petitTexte">Gestion BD</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
