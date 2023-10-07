e5e296bc1685b8961b7c11857fa43f4206af9254
Tordeux Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-10-07 17:19:26 +0200

    update visuel

--- src/vue/Entreprise/vueCompteEntreprise.php
+++ src/vue/Entreprise/vueCompteEntreprise.php
@@ -9,25 +9,24 @@
             <h3>Bonjour, Entreprise A</h3>
             <p>Voici les informations sur votre compte entrprise :</p>
         </div>
-        <div class="listInfosEntr">
-            <ul id="infosEntr">
-                <li>Siret : 11111111111111</li>
-                <li>nom entreprise : Entreprise A</li>
-                <li>Statut juridique : SA</li>
-                <li>Effectif : 10</li>
-                <li>CodeNAF : 6205A</li>
-                <li>Tel : 01 32 79 12 43</li>
-                <li>Adresse : 32 rue des Halles</li>
-                <li>Ville : Montpellier</li>
-            </ul>
-        </div>
         <div class="imageBienvenue">
             <img src="../ressources/images/bienvenueRemoved.png" alt="image de bienvenue" class="imageMoyenne">
         </div>
     </div>
-    <div>
+    <div class="informationsEtudiant">
+        <ul id="infosEntr">
+            <li>Siret : 11111111111111</li>
+            <li>nom entreprise : Entreprise A</li>
+            <li>Statut juridique : SA</li>
+            <li>Effectif : 10</li>
+            <li>CodeNAF : 6205A</li>
+            <li>Tel : 01 32 79 12 43</li>
+            <li>Adresse : 32 rue des Halles</li>
+            <li>Ville : Montpellier</li>
+        </ul>
+    </div>
+    </div>
 
 
-</div>
 </body>
 </html>
