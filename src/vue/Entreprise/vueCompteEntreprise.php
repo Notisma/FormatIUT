f44cd1d82e6ee2108b04b8ebe41a60c41db6a8ea
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-10-10 18:18:42 +0200

    Avancées sur vueCompteEntreprise.php

--- src/vue/Entreprise/vueCompteEntreprise.php
+++ src/vue/Entreprise/vueCompteEntreprise.php
@@ -13,9 +13,9 @@
             <div class="texteAGauche">
                 <p>Changez votre logo ici :</p>
                 <form enctype="multipart/form-data" action="#" method="post">
-                    <input type="hidden" name="MAX_FILE_SIZE" value="250000" />
-                    <input type="file" name="fic" size=50 />
-                    <input type="submit" value="Envoyer" />
+                    <input type="hidden" name="MAX_FILE_SIZE" value="250000"/>
+                    <input type="file" name="fic" size=50/>
+                    <input type="submit" value="Envoyer"/>
                 </form>
             </div>
             <div class="imageEntre">
@@ -26,25 +26,36 @@
 
     <div class="conteneurBienvenueEntr">
         <div class="texteBienvenue">
-            <h3>Bonjour, voici votre compte entreprise</h3>
-            <p>Voici les informations sur votre compte entreprise :</p>
+            <h3>Bonjour, bienvenue sur votre compte entreprise</h3>
+            <p>Visualisez les données de votre compte et modifiez-les sur la même page</p>
+            <p>Voici toutes les informations sur votre compte entreprise :</p>
         </div>
         <div class="imageBienvenue">
+            <img src="../ressources/images/parametresEntr.png" alt="image de bienvenue">
         </div>
     </div>
 
 
     <div class="informationsActuellesEntr">
-        <ul id="infosEntr">
-            <li>Siret : 11111111111111</li>
-            <li>nom entreprise : Entreprise A</li>
+        <h3>Vos Informations Actuelles</h3>
+        <div class="infosActu">
+            <ul id="infosEntr">
+                <?php
+                echo "
+            <li>Siret : 10474930</li>
+            <li>Nom : Entreprise A</li>
             <li>Statut juridique : SA</li>
             <li>Effectif : 10</li>
             <li>CodeNAF : 6205A</li>
-            <li>Tel : 01 32 79 12 43</li>
+            <li>Téléphone : 01 32 79 12 43</li>
             <li>Adresse : 32 rue des Halles</li>
             <li>Ville : Montpellier</li>
-        </ul>
+            " ?>
+            </ul>
+
+            <img src="../ressources/images/infosEntre.png" alt="illu">
+
+        </div>
     </div>
 
 
