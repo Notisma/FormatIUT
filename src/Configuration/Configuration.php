d7b23c52963ef360416d2cee03b6f21af65f5158
Richard Igor <raphael.izoret@etu.umontpellier.fr>
2023-10-13 16:10:45 +0200

    vueErreur

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -9,12 +9,23 @@ define('DB_NAME', 'loyet'); //Nom de la base de données4
 namespace App\FormatIUT\Configuration;
 
 class Configuration{
-    static private array $config= array(
-        'hostname'=>'localhost',
-        'database'=>'touzersae',
-        'login'=>'root',
-        'password'=>'root',
-        'port' =>'3306'
+    static private array $config = array(
+        // webinfo à l'IUT
+        // localhost sur votre machine
+        // webinfo.iutmontp.univ-montp2.fr pour accéder à webinfo depuis l'extérieur
+        'hostname' => 'webinfo',
+        // A l'IUT, vous avez une BDD nommee comme votre login
+        // Sur votre machine, vous devrez creer une BDD
+        'database' => 'loyet',
+        // À l'IUT, le port de MySQL est particulier : 3316
+        // Ailleurs, on utilise le port par défaut : 3306
+        'port' => '3316',
+        // A l'IUT, c'est votre login
+        // Sur votre machine, vous avez surement un compte 'root'
+        'login' => 'loyet',
+        // A l'IUT, c'est votre mdp (INE par defaut)
+        // Sur votre machine personelle, vous avez créé ce mdp a l'installation
+        'password' => '26032004'
     );
 
     public static function getHostname(): string
