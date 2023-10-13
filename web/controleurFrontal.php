4df8a6fa6bdec2b24163f8757a8cdcac54bd7800
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-10-13 15:45:09 +0200

    Réparation (encore)

--- web/controleurFrontal.php
+++ web/controleurFrontal.php
@@ -1,12 +1,14 @@
 <?php
 
 require_once __DIR__ . '/../src/Lib/Psr4AutoloaderClass.php';
+
 // initialisation
 $loader = new App\FormatIUT\Lib\Psr4AutoloaderClass();
 $loader->register();
-// enregistrement d'une association "espace de nom" → "dossier"
+// enregistrement d'une association "namespace" → "dossier"
 $loader->addNamespace('App\FormatIUT', __DIR__ . '/../src');
 
+use App\FormatIUT\Controleur\ControleurMain as CGlobal;
 
 
 // On recupère l'action passée dans l'URL
