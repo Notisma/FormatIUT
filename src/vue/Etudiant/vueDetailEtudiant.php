9b8826c5f8833e5fb299034d7876488a2a18b2ba
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-10-14 11:37:39 +0200

    Début des travaux sur vueDetailEtudiant.php

--- src/vue/Etudiant/vueDetailEtudiant.php
+++ src/vue/Etudiant/vueDetailEtudiant.php
@@ -1,74 +1,84 @@
-<link rel="stylesheet" href="../ressources/css/styleVueIndex.css">
+<html>
+<head>
+    <link rel="stylesheet" href="../ressources/css/styleVueDetailEtudiant.css">
+</head>
 <body>
-<div id="center">
-    <!-- Partie concernant l'offre-->
-    <div id="offre">
-        <h1><?php
-
-            use App\FormatIUT\Controleur\ControleurEtuMain;
-            use App\FormatIUT\Modele\Repository\EtudiantRepository;
-
-            $nomHTML = htmlspecialchars($offre->getNomOffre());
-            echo $nomHTML;
-            ?></h1>
-        <h4>
-            <?php
-            echo $offre->getTypeOffre();
-            ?>
-        </h4>
-        <p>
-            <?php
-            $dateDebut = date_format($offre->getDateDebut(), 'd F Y');
-            $dateFin = date_format($offre->getDateFin(), 'd F Y');
-            $duree = ($offre->getDateDebut()->diff($offre->getDateFin()))->format('%m mois, %d jours');
-            echo "Du " . $dateDebut . " au " . $dateFin . " : " . $duree;
-            ?>
-        </p>
-        <p>
-            <?php
-            echo "Gratification : " . $offre->getGratification();
-            echo "<br>Durée en heure : " . $offre->getDureeHeures();
-            echo "<br>Nombre de jours par semaine : " . $offre->getJoursParSemaine();
-            echo "<br>Nombre d'heure Hebdomadaire : " . $offre->getNbHeuresHebdo();
-            ?>
-        </p>
-        <p>
-            <?php echo $offre->getSujet(); ?>
-        </p>
+<div class="boiteMain">
+    <div class="conteneurBienvenueDetailEntr">
+        <div class="texteBienvenue">
+            <!-- affhichage des informations principales de l'offre -->
+            <h2><?php echo $offre->getNomOffre()?></h2>
+            <h4><?php echo "Du ".date_format($offre->getDateDebut(),'d F Y'). " au ".date_format($offre->getDateFin(),'d F Y')?></h4>
+            <p><?php  echo ($offre->getDateDebut()->diff($offre->getDateFin()))->format('Durée : %m mois, %d jours.'); ?></p>
+        </div>
+        <div class="imageBienvenue">
+            <img src="../ressources/images/entrepriseOffre.png" alt="image de bienvenue">
+        </div>
     </div>
-    <!-- Partie concernant l'entreprise qui propose cet offre-->
-    <div id="entreprise">
-        <h3>
-            <?php echo $entreprise->getNomEntreprise(); ?>
-        </h3>
-        <p>
-            Tel : <?php echo $entreprise->getTel() ?>
-            <br>
-            Adresse : <?php echo $entreprise->getAdresse() ?>
-        </p>
-    </div>
-<?php
-$formation = ((new \App\FormatIUT\Modele\Repository\FormationRepository())->estFormation($offre));
 
-$idOffreURl = rawurlencode($offre->getIdOffre());
-//est-ce que l'offre est déjà assigné ?
-if (!is_null($formation)) {
-    //si oui, est-ce cet étudiant qui y est assigné ?
-    if ($formation->getIdEtudiant() == ControleurEtuMain::getCleEtudiant()) {
-        echo "Vous avez été accepté pour cette formation";
-    } else {
-        echo "Cet Offre est déjà assigné";
-    }
-} else {
-    //si l'offre est disponible, l'étudiant a-t-il déjà une formation ?
-    if (!(new EtudiantRepository())->aUneFormation(ControleurEtuMain::getCleEtudiant())) {
-        //si non, l'étudiant y-a-til déjà postulé ?
-        if ((new EtudiantRepository())->aPostuler(ControleurEtuMain::getCleEtudiant(), $offre->getIdOffre())) {
-            echo "Vous avez déjà postuler";
-        } else {
-            echo "<a href='?controleur=EtuMain&action=postuler&idOffre=" . $idOffreURl . "''><button>Postuler</button></a>";
-        }
-    } else {
-        echo "Vous avez déjà une formation";
-    }
-}
+    <!-- TODO finir de lier à la BD -->
+    <div class="infosOffreEntr">
+        <h3>Les Informations de l'Offre</h3>
+        <div class="petitConteneurInfosOffre">
+            <div class="overflowListe">
+                <div class="overflowListe2">
+                    <div id="liseInfosOffreEntr">
+                        <p><span>Rémunération :</span> <?php echo $offre->getGratification() ?>€ par mois</p>
+                        <p><span>Durée en heures :</span> <?php echo $offre->getDureeHeures() ?> heures au total</p>
+                        <p><span>Nombre de jours par semaines :</span> <?php echo $offre->getJoursParSemaine()?> jours</p>
+                        <p><span>Nombre d'Heures hebdomadaires :</span> <?php echo $offre->getNbHeuresHebdo() ?> heures</p>
+                        <p><span>Détails de l'offre :</span> <?php echo $offre->getDetailProjet() ?></p>
+                    </div>
+                </div>
+            </div>
+            <img src="../ressources/images/entrepriseData.png" alt="illu">
+        </div>
+    </div>
+
+
+    <div class="actionsRapidesEntr">
+        <h3>Actions Rapides</h3><a href="?controleur=EntrMain&action=supprimerOffre&idOffre=">
+            <button class='boutonAssigner'>POSTULER</button>
+        </a>
+        <a href='?action=afficherAccueilEtu&controleur=EtuMain'>
+            <button class='boutonAssigner'>RETOUR</button>
+        </a>
+    </div>
+
+
+    <div class="listeEtudiantsPostulants">
+        <h3>Nombre d'étudiants postulants</h3>
+
+        <div class="wrapPostulants">
+            <?php
+            $listeEtu=((new \App\FormatIUT\Modele\Repository\EtudiantRepository())->EtudiantsEnAttente($offre->getIdOffre()));
+            if (empty($listeEtu)){
+                echo "
+                <div class='erreur'>
+                <h4>Personne n'a postulé. Faites Vite !</h4>
+                <img src='../ressources/images/erreur.png' alt='erreur'>
+                </div>
+                ";
+            }else {
+                echo "
+                <div class='nbPostulants'>
+                <img src='../ressources/images/equipe.png' alt='postulants'>
+                <h4>5 Etudiants ont déjà postulé.</h4>
+                </div>          
+                
+                
+            ";
+            }
+
+            ?>
+
+        </div>
+
+    </div>
+
+
+</div>
+
+
+</body>
+</html>
