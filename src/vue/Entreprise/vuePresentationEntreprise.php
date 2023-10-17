691b283a4381e32c208c9968ea5ac98a4b9ff1a3
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-10-17 17:29:41 +0200

    Améliorations visuelles

--- src/vue/Entreprise/vuePresentationEntreprise.php
+++ src/vue/Entreprise/vuePresentationEntreprise.php
@@ -4,8 +4,6 @@
 </head>
 
 <body>
-<p>test</p>
-</body>
 
 
 <div class="wrapComplet">
@@ -14,20 +12,18 @@
         <div class="formulaireGauchePE">
             <form action="controleurFrontal.php?controleur=EntrMain&action=creerCompteEntreprise" method="post">
                 <h1>Créez votre compte Entreprise</h1>
-                <div class="wrapInput">
-                    <input type="text" name="siteWeb" placeholder="SIRET de l'entreprise" required>
-                    <input type="text" name="nomEntreprise" placeholder="Nom de l'entreprise" required>
-                    <input type="text" name="adresse" placeholder="Adresse de l'entreprise" required>
-                    <input type="text" name="codePostal" placeholder="Code Postal" required>
-                    <input type="text" name="ville" placeholder="Ville" required>
-                    <input type="text" name="tel" placeholder="Téléphone" required>
-                    <input type="text" name="statutJuridique" placeholder="Statut Juridique" required>
-                    <input type="text" name="effectif" placeholder="Effectif" required>
-                    <input type="text" name="codeNaf" placeholder="Code NAF" required>
-                    <input type="text" name="mdp" placeholder="Mot de passe" required>
-                    <input type="text" name="mdpConf" placeholder="Confirmer le mot de passe" required>
-                </div>
-                <input type="submit" value="Créer le compte">
+                <input type="text" name="siteWeb" placeholder="SIRET de l'entreprise" required>
+                <input type="text" name="nomEntreprise" placeholder="Nom de l'entreprise" required>
+                <input type="text" name="adresse" placeholder="Adresse de l'entreprise" required>
+                <input type="text" name="codePostal" placeholder="Code Postal" required>
+                <input type="text" name="ville" placeholder="Ville" required>
+                <input type="text" name="tel" placeholder="Téléphone" required>
+                <input type="text" name="statutJuridique" placeholder="Statut Juridique" required>
+                <input type="text" name="effectif" placeholder="Effectif" required>
+                <input type="text" name="codeNaf" placeholder="Code NAF" required>
+                <input type="text" name="mdp" placeholder="Mot de passe" required>
+                <input type="text" name="mdpConf" placeholder="Confirmer le mot de passe" required>
+                <input type="submit" class="valider" value="Créer le compte">
             </form>
         </div>
 
@@ -38,7 +34,7 @@
 
 
 </div>
-
+</body>
 
 
 </html>
