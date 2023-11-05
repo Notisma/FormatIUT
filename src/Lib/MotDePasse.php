e68f57372103d35173c8d687cc76914541bf866c
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-05 10:40:50 +0100

    Correction des nonces et des emails mdp oubliés

--- src/Lib/MotDePasse.php
+++ src/Lib/MotDePasse.php
@@ -24,9 +24,10 @@ class MotDePasse
     {
         //TODO : vérifier que ce qui est généré n'est pas déjà pris dans la base de données
 
-        //ne doit générer que des chiffres et des lettres, pas de caractères spéciaux
         $octetsAleatoires = random_bytes(ceil($nbCaracteres * 6 / 8));
-        return substr(base64_encode($octetsAleatoires), 0, $nbCaracteres);
+        $chaineAleatoire = bin2hex($octetsAleatoires);
+        $chaineAleatoire = preg_replace('/[^0-9a-zA-Z]/', '', $chaineAleatoire);
+        return substr($chaineAleatoire, 0, $nbCaracteres);
     }
 
 }
