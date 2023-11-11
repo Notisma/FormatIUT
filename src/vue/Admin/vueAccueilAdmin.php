a019444f17d6ff01e66e7dcaf50e9444de2c2dad
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-11 10:38:05 +0100

    Feat - Avancées sur accueil ADMIN 3

--- src/vue/Admin/vueAccueilAdmin.php
+++ src/vue/Admin/vueAccueilAdmin.php
@@ -54,6 +54,25 @@
 
     <div class="wrapAdminEntr">
         <h3 class="titre">Alertes - Entreprises</h3>
+        <div class="wrapAlertes">
+            <!-- exemple d'alerte -->
+            <a href="tt" class="alerteEntr">
+                <div class="imageAlerte">
+                    <img src="../ressources/images/logo_CA.png" alt="image">
+                </div>
+
+                <div class="contenuAlerte">
+                    <h3 class="titre" id="rouge">CRÉDIT AGRICOLE - Demande de création de compte</h3>
+                    <div class="sujetAlerte">
+                        <img src="../ressources/images/attention.png" alt="image">
+                        <p>Demande de création de compte le 11/11/2023</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="wrapBoutons">
+            <a href="">VOIR PLUS</a>
+        </div>
     </div>
 
     <div class="wrapAdminEtu">
