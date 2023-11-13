e55508257d3ee762fe4e3231ef36a48d1ae11ef5
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-13 10:13:37 +0100

    Feat - Avancées sur accueil ADMIN 6

--- src/vue/Admin/vueAccueilAdmin.php
+++ src/vue/Admin/vueAccueilAdmin.php
@@ -55,7 +55,7 @@
     <div class="wrapAdminEntr">
         <h3 class="titre">Alertes - Entreprises</h3>
         <div class="wrapAlertes">
-            <!-- exemple d'alerte -->
+            <!-- exemple d'alerte - compte créé -->
             <a href="tt" class="alerteEntr">
                 <div class="imageAlerte">
                     <img src="../ressources/images/logo_CA.png" alt="image">
@@ -65,11 +65,26 @@
                     <h3 class="titre" id="rouge">CRÉDIT AGRICOLE - Demande de création de compte</h3>
                     <div class="sujetAlerte">
                         <img src="../ressources/images/attention.png" alt="image">
-                        <p>Compte créé</p>
                         <p>Demande de création de compte le 11/11/2023</p>
                     </div>
                 </div>
             </a>
+
+            <!-- exemple d'alerte - offre postée -->
+            <a href="tt" class="alerteEntr">
+                <div class="imageAlerte">
+                    <img src="../ressources/images/logo_CA.png" alt="image">
+                </div>
+
+                <div class="contenuAlerte">
+                    <h3 class="titre" id="rouge">CRÉDIT AGRICOLE - Offre en attente</h3>
+                    <div class="sujetAlerte">
+                        <img src="../ressources/images/attention.png" alt="image">
+                        <p>Demande d'envoi d'une offre le 13/11/2023</p>
+                    </div>
+                </div>
+            </a>
+
         </div>
         <div class="wrapBoutons">
             <a href="">VOIR PLUS</a>
@@ -78,9 +93,50 @@
 
     <div class="wrapAdminEtu">
         <h3 class="titre">Alertes - Étudiants</h3>
+
+        <div class="wrapAlertes">
+
+            <!-- exemple d'alerte - compte créé -->
+            <a href="tt" class="alerteEntr" id="hoverRose">
+                <div class="imageAlerte">
+                    <img src="../ressources/images/profil.png" alt="image">
+                </div>
+
+                <div class="contenuAlerte">
+                    <h3 class="titre" id="rouge">Romain TOUZÉ</h3>
+                    <p>2e année - RACDV - Q2</p>
+                    <div class="sujetAlerte">
+                        <img src="../ressources/images/attention.png" alt="image">
+                        <p>Aucun Stage/Alternance</p>
+                    </div>
+                </div>
+            </a>
+
+            <!-- un exemple différent -->
+            <a href="tt" class="alerteEntr" id="hoverRose">
+                <div class="imageAlerte">
+                    <img src="../ressources/images/profil.png" alt="image">
+                </div>
+
+                <div class="contenuAlerte">
+                    <h3 class="titre" id="rouge">Thomas LOYE</h3>
+                    <p>2e année - RACDV - Q2</p>
+                    <div class="sujetAlerte">
+                        <img src="../ressources/images/attention.png" alt="image">
+                        <p>A modifié sa convention le 13/11/2023</p>
+                    </div>
+                </div>
+            </a>
+
+
+
+        </div>
+
+        <div class="wrapBoutons">
+            <a href="">VOIR PLUS</a>
+        </div>
+
     </div>
-
-
 
 
 </div>
