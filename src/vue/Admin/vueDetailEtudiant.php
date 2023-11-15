4795bb4d207b4abae69782429bcacc83b6df9373
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-15 22:50:59 +0100

    feat : avancée vue detail etu ADMINS

--- src/vue/Admin/vueDetailEtudiant.php
+++ src/vue/Admin/vueDetailEtudiant.php
@@ -1,3 +1,27 @@
+<?php
+
+use App\FormatIUT\Modele\Repository\EtudiantRepository;
+
+$etudiant = (new EtudiantRepository())->getObjectParClePrimaire($_GET["numEtu"]);
+?>
+
 <div class="wrapCentreEtu">
-    <p></p>
+    <div class="gaucheEtu">
+        <div class="infosEtu">
+            <?php
+            echo "<img src='data:image/jpeg;base64," . base64_encode($etudiant->getImg()) . "' alt='etudiant'>";
+            echo "<h1 id='rouge' class='titre'>" . $etudiant->getPrenomEtudiant() . " " . $etudiant->getNomEtudiant() . "</h1>";
+            echo "<h3 class='titre'>" . $etudiant->getGroupe() . " - " . $etudiant->getParcours() . "</h3>";
+            ?>
+        </div>
+
+        <div class="wrapBoutons">
+            <a href="?action=supprimerEtudiant&controleur=AdminMain&numEtu=<?php echo $etudiant->getNumEtudiant() ?>">Supprimer</a>
+        </div>
+
+    </div>
+
+    <div class="droiteEtu">
+        <p></p>
+    </div>
 </div>
