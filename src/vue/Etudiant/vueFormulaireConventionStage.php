651215d9c9892b77da4ffab44ca84d7bc1cd1777
Jean Hubert <izoret.raphael@gmail.com>
2023-11-23 00:01:17 +0100

    chore: main et etumain

--- src/vue/Etudiant/vueFormulaireConventionStage.php
+++ src/vue/Etudiant/vueFormulaireConventionStage.php
@@ -77,7 +77,7 @@
                 </div>
                 </p>
                 <input type="hidden" value="<?=date("d-m-Y");?>" name="dateCreation">
-                <input type="submit" value="Envoyer" formaction="?action=creationConvention&controleur=EtuMain">
+                <input type="submit" value="Envoyer" formaction="?action=creerConvention&controleur=EtuMain">
             </fieldset>
         </form>
     </div>
