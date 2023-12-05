fc82f496e68a83875a3d37389f3a988d2607e5b7
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-05 19:59:56 +0100

    feat : ajout du menu compte Admin

--- src/vue/Admin/vueCompteAdmin.php
+++ src/vue/Admin/vueCompteAdmin.php
@@ -1,24 +1,36 @@
 <div class="centreCompte">
     <div class="menuAdmin">
         <div class="sousMenuAdmin">
-            <h3 class="titre">Mon Compte</h3>
+            <img src="../ressources/images/profil.png" alt="profil">
+            <div>
+                <h3 class="titre">Mon Compte</h3>
+            </div>
         </div>
 
         <div class="sousMenuAdmin">
-            <h3 class="titre">Mes Notifications</h3>
+            <img src="../ressources/images/notif.png" alt="profil">
+            <div>
+                <h3 class="titre">Mes Notifications</h3>
+            </div>
         </div>
 
         <div class="sousMenuAdmin">
-            <h3 class="titre">Mes Collègues</h3>
+            <img src="../ressources/images/professeur.png" alt="profil">
+            <div>
+                <h3 class="titre">Mes Collègues</h3>
+            </div>
         </div>
 
         <div class="sousMenuAdmin">
-            <h3 class="titre">Mes Étudiants</h3>
+            <img src="../ressources/images/etudiants.png" alt="profil">
+            <div>
+                <h3 class="titre">Mes Étudiants</h3>
+            </div>
         </div>
     </div>
 
     <div class="mainAdmins">
-
+        <p>test</p>
     </div>
 
 </div>
