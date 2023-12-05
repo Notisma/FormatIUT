4e0b0b787b346548390a2daa3ca733b83d9ce0b6
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-05 22:48:52 +0100

    feat : menu "mon Compte" (FRONT) terminé

--- src/vue/Admin/vueCompteAdmin.php
+++ src/vue/Admin/vueCompteAdmin.php
@@ -1,5 +1,12 @@
+<?php
+
+use App\FormatIUT\Configuration\Configuration;
+
+$admin = (new \App\FormatIUT\Modele\Repository\ProfRepository())->getObjectParClePrimaire(\App\FormatIUT\Lib\ConnexionUtilisateur::getLoginUtilisateurConnecte());
+?>
+
 <div class="centreCompte">
-    <script>window.onload = function() {
+    <script>window.onload = function () {
             afficherPageCompte("compte");
         };</script>
     <div class="menuAdmin">
@@ -33,7 +40,36 @@
     </div>
 
     <div class="mainAdmins" id="compte">
-        <p>compte</p>
+        <h2 class="titre" id="rouge">Modifier mon Profil</h2>
+        <form method="POST">
+            <h3 class="titre">Mon Avatar</h3>
+            <div class="avatar">
+                <img src="../ressources/images/admin.png" alt="avatar">
+                <div>
+                    <input type="file" name="avatar" id="avatar_id" accept="image/png/jpeg, image/jpeg">
+                    <p>Glissez-déposez un fichier ou parcourez vos fichiers. JPEG et PNG uniquement</p>
+                </div>
+            </div>
+
+            <h3 class="titre">Nom</h3>
+            <div class="inputCentre">
+                <input type="text" value='<?= htmlspecialchars($admin->getNomProf()); ?>' name="nom" id="nom_id"
+                       required maxlength="50"/>
+            </div>
+
+            <h3 class="titre">Prénom</h3>
+            <div class="inputCentre">
+                <input type="text" value='<?= htmlspecialchars($admin->getPrenomProf()); ?>' name="prenom"
+                       id="prenom_id" required maxlength="50"/>
+            </div>
+
+            <div class="inputCentre">
+                <input type="submit" value="Enregistrer" name="modifier" id="modifier_id"/>
+            </div>
+
+
+        </form>
+
     </div>
 
     <div class="mainAdmins" id="notifs">
