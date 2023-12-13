8f767be43bfeed70bc2a382c74da883af3d3c213
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-13 11:15:04 +0100

    fix : correction de crash (encore)

--- src/vue/Admin/vueDetailEntreprise.php
+++ src/vue/Admin/vueDetailEntreprise.php
@@ -31,7 +31,7 @@
             <?php
             if (\App\FormatIUT\Lib\ConnexionUtilisateur::getTypeConnecte()=="Administrateurs") {
                 if ($entreprise->isEstValide()) {
-                    echo '<a href="?action=supprimerEntreprise&controleur=AdminMain&siret=<?php echo $entreprise->getSiret() ?>">SUPPRIMER</a>';
+                    echo '<a href="?action=supprimerEntreprise&controleur=AdminMain&siret='. $entreprise->getSiret() .'">SUPPRIMER</a>';
                 } else {
                     echo '<a href="?action=refuserEntreprise&controleur=AdminMain&siret=' . $entreprise->getSiret() . '">REFUSER</a>';
                     echo '<a id="vert" href="?action=validerEntreprise&controleur=AdminMain&siret=' . $entreprise->getSiret() . '">ACCEPTER</a>';
