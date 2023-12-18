596c7b614421ed4794f1864bec903c3711d1aada
TouzeR <romain.touze@etu.umontpellier.fr>
2023-12-18 15:06:36 +0100

    feature : users

---
+++ src/Lib/Users/Utilisateur.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\FormatIUT\Lib\Users;
+
+abstract class Utilisateur
+{
+    private string $login;
+
+    /**
+     * @return string
+     */
+    public function __construct(string $login)
+    {
+        $this->login=$login;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public abstract function getRecherche():array;
+
+    public abstract function getControleur():string;
+    public abstract function getImageProfil();
+    public abstract function getTypeConnecte() :string;
+
+
+}
