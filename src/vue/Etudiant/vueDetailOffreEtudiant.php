32c62cda0d119cfeb16f8bc999abee037b37c151
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-20 18:42:11 +0100

    feat : detailOffreEtu en cours

--- src/vue/Etudiant/vueDetailOffreEtudiant.php
+++ src/vue/Etudiant/vueDetailOffreEtudiant.php
@@ -1,8 +1,12 @@
 <?php
 use App\FormatIUT\Configuration\Configuration;
 $etudiant = (new \App\FormatIUT\Modele\Repository\EtudiantRepository())->getObjectParClePrimaire(\App\FormatIUT\Lib\ConnexionUtilisateur::getNumEtudiantConnecte());
+$offre = (new \App\FormatIUT\Modele\Repository\FormationRepository())->getObjectParClePrimaire($_GET['idFormation']);
 ?>
 
 <div class="detailOffreEtu">
-    <p>t</p>
+
+
+
+
 </div>
