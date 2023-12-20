8a033b92a62af22a64e0662172f4e012e2d92b43
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-20 18:37:00 +0100

    feat : refonte detailOffreEtu - début

---
+++ src/vue/Etudiant/vueDetailOffreEtudiant.php
@@ -0,0 +1,8 @@
+<?php
+use App\FormatIUT\Configuration\Configuration;
+$etudiant = (new \App\FormatIUT\Modele\Repository\EtudiantRepository())->getObjectParClePrimaire(\App\FormatIUT\Lib\ConnexionUtilisateur::getNumEtudiantConnecte());
+?>
+
+<div class="detailOffreEtu">
+    <p>t</p>
+</div>
