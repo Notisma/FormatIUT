aececbb33cdcc62f33ab103e0cb1a81f3fe6aeef
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-21 19:01:50 +0100

    feat : detailOffreEtu -> partie offre OK

--- src/vue/Etudiant/vueDetailOffreEtudiant.php
+++ src/vue/Etudiant/vueDetailOffreEtudiant.php
@@ -18,6 +18,19 @@ $entreprise = (new \App\FormatIUT\Modele\Repository\EntrepriseRepository())->get
                 <?php echo htmlspecialchars((new App\FormatIUT\Modele\Repository\VilleRepository())->getObjectParClePrimaire($entreprise->getIdVille())->getNomVille()) ?></h3>
         </div>
 
+        <div class="offre">
+            <h2 class="titre" id="rouge">Description de l'offre :</h2>
+            <h3 class="titre"><?php echo htmlspecialchars($offre->getNomOffre()) ?> : <?php echo htmlspecialchars($offre->getSujet())?> - <?php echo htmlspecialchars($offre->getTypeOffre()) ?></h3>
+            <h4 class="titre"><?php echo "Du " . htmlspecialchars($offre->getDateDebut()) . " au " . htmlspecialchars($offre->getDateFin()) ?></h4>
+            <h4 class="titre">Rémunération : <?php echo $offre->getGratification() ?>€ par mois</h4>
+            <h4 class="titre">Durée en heures : <?php echo $offre->getDureeHeure() ?> heures au total</h4>
+            <h4 class="titre">Nombre de jours par semaines : <?php echo $offre->getJoursParSemaine() ?> jours</h4>
+            <h4 class="titre">Nombre d'Heures hebdomadaires : <?php echo $offre->getNbHeuresHebdo() ?> heures</h4>
+            <h5 class="titre">Détails de l'offre : <?php $detailHTML = htmlspecialchars($offre->getDetailProjet());
+                echo $detailHTML ?>
+            </h5>
+        </div>
+
     </div>
 
     <div class="actionsOffre">
