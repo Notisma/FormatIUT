257dfb264a3d8863c0ae87516ab54592b96e3aab
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-23 19:08:30 +0100

    fix : correction du popup de première connexion

--- src/vue/Etudiant/vueAccueilEtudiant.php
+++ src/vue/Etudiant/vueAccueilEtudiant.php
@@ -67,3 +67,134 @@ $etudiant = (new \App\FormatIUT\Modele\Repository\EtudiantRepository())->getObje
 </div>
 
 
+<div class="premiereCo" id="popupPremiereCo">
+
+    <div id="0" class="enfant">
+        <div class="imagePremiereCo">
+            <img src="../ressources/images/0.png" alt="image">
+            <h2>COMPLETEZ VOTRE PROFIL AVANT DE COMMENCER</h2>
+        </div>
+        <div class="contenuPremiereCo">
+            <h3>Pour avoir plus de visibilité pour les entreprises</h3>
+            <p>En quelques clics, complétez votre profil, puis démarrez l'aventure Format'IUT !</p>
+
+            <div class="wrapBoutons">
+                <a href="?action=seDeconnecter">RETOUR</a>
+                <a onclick="afficherPopupPremiereCo(1)">SUIVANT</a>
+            </div>
+        </div>
+    </div>
+
+    <div id="1" class="enfant">
+        <div class="imagePremiereCo">
+            <img src="../ressources/images/mesInfos.jpg" alt="image">
+            <h2>MES INFORMATIONS</h2>
+        </div>
+        <div class="contenuPremiereCo">
+            <form method="post" action="../web/controleurEtuMain.php" onsubmit="afficherPopupPremiereCo(2)">
+                <label for="numEtu">Numéro étudiant :
+                    <input type="number" name="numEtu" placeholder="11102117" required>
+                </label>
+
+
+                <label for="sexe">Sexe :
+                    <select name="sexe" required>
+                        <option value="M">Homme</option>
+                        <option value="F">Femme</option>
+                        <option value="X">Je préfère ne pas répondre</option>
+                    </select>
+                </label>
+                <?php
+                $ancienNumEtu = $etudiant->getNumEtudiant();
+                ?>
+
+                <div class="wrapBoutons">
+                    <a onclick="afficherPopupPremiereCo(0)">RETOUR</a>
+                    <input type="hidden" name="oldNumEtu" value="<?php echo $ancienNumEtu ?>">
+                    <input type="hidden" name="controleur" value="EtuMain">
+                    <input type="submit" value="SUIVANT" formaction="?action=setNumEtuSexe">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div id="2" class="enfant">
+        <div class="imagePremiereCo">
+            <img src="../ressources/images/mesContacts.jpg" alt="image">
+            <h2>MES CONTACTS</h2>
+        </div>
+        <div class="contenuPremiereCo">
+            <form method="post" action="../web/controleurEtuMain.php" onsubmit="afficherPopupPremiereCo(3)">
+                <label for="telephone">Téléphone :
+                    <input type="number" name="telephone" placeholder="0670809010">
+                </label>
+
+                <label for="telephone">Mail personnel :
+                    <input type="email" name="mailPerso" placeholder="exemple@exemple.ex">
+                </label>
+
+                <?php
+                $numEtu = $etudiant->getNumEtudiant();
+                ?>
+
+                <div class="wrapBoutons">
+                    <a onclick="afficherPopupPremiereCo(1)">RETOUR</a>
+                    <input type="hidden" name="numEtu" value="<?php echo $numEtu ?>">
+                    <input type="hidden" name="controleur" value="EtuMain">
+                    <input type="submit" value="SUIVANT" formaction="?action=setTelMailPerso">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="enfant" id="3">
+        <div class="imagePremiereCo">
+            <img src="../ressources/images/maFormation.jpg" alt="image">
+            <h2>MA FORMATION</h2>
+        </div>
+        <div class="contenuPremiereCo">
+            <form method="post" action="../web/controleurEtuMain.php" onsubmit="afficherPopupPremiereCo(4)">
+                <label for="groupe">Groupe de TD :
+                    <input type="text" name="groupe" placeholder="Q1" required>
+                </label>
+
+                <label for="parcours">Parcours :
+                    <input type="text" name="parcours" placeholder="RACDV" required>
+                </label>
+
+                <div class="wrapBoutons">
+                    <a onclick="afficherPopupPremiereCo(2)">RETOUR</a>
+                    <input type="hidden" name="numEtu" value="<?php echo $numEtu ?>">
+                    <input type="hidden" name="controleur" value="EtuMain">
+                    <input type="submit" value="SUIVANT" formaction="?action=setGroupeParcours">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="enfant" id="4">
+        <div class="imagePremiereCo">
+            <img src="../ressources/images/maFormation.jpg" alt="image">
+            <h2>MA PHOTO DE PROFIL (FACULTATIF)</h2>
+        </div>
+        <div class="contenuPremiereCo">
+            <form enctype="multipart/form-data" action="?action=updateImage&controleur=EtuMain" method="post">
+                <input type="hidden" name="MAX_FILE_SIZE" value="1000000"/>
+                <input type="file" name="fic" size=500/>
+
+                <h4>Cliquez sur "Terminer" pour enregistrer vos informations et commencer l'aventure Format'IUT !</h4>
+
+                <div class="wrapBoutons">
+                    <a onclick="afficherPopupPremiereCo(3)">RETOUR</a>
+                    <input type="hidden" name="numEtu" value="<?php echo $numEtu ?>">
+                    <input type="hidden" name="estPremiereCo" value="true">
+                    <input type="submit" value="TERMINER" onclick="fermerPopupPremiereCo()"/>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+</div>
+
+
