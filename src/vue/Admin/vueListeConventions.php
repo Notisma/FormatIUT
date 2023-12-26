f278de1eaa8e3cff6bad113666786a00aea7436e
Tordeux Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-12-26 02:15:42 +0100

    feature : Ajout de la fonctionnalité liste convention

---
+++ src/vue/Admin/vueListeConventions.php
@@ -0,0 +1,50 @@
+<div class="wrapCentreListEntr">
+    <div class="gauche">
+        <h2 class="titre">Liste des Entreprises :</h2>
+
+        <div class="wrapEntreprises">
+            <?php
+            $erreur = 0;
+
+            foreach ($listeFormations as $convention) {
+                if ($convention->getDateCreationConvention() != null && !$convention->getConventionValidee()) {
+                    $erreur++;
+                    echo "
+                        <a href='?action=afficherDetailConvention&controleur=AdminMain&numEtudiant=" . $convention->getIdEtudiant() . "' class='entreprise'>
+                        
+                        
+                        <div class='entrepriseDroite'>
+                            <h3 class='titre' id='rouge'>Etudiant : " . htmlspecialchars($convention->getIdEtudiant()) . "</h3>
+                            <p>Convention ". $convention->getTypeOffre() ." à valider.</p>";
+
+
+                    echo "
+                        
+                        </div>                        
+                        </a>
+                        
+                    ";
+                }
+            }
+
+            if ($erreur == 0) {
+                echo "<div class='wrapErreur'>";
+                echo "<img src='../ressources/images/erreur.png' alt='erreur'>";
+                echo "<h3 class='titre'>Aucune convention à valider</h3>";
+                echo "</div>";
+            }
+            ?>
+        </div>
+
+
+    </div>
+
+    <div class="droite">
+        <img src="../ressources/images/entrepriseAdmins.png" alt="admin">
+        <h3 class="titre" id="rouge">Toutes les entreprises de la base de données</h3>
+        <h4 class="titre">Consultez le statut de chaque entreprise en un coup d'oeil</h4>
+        <p>Cliquez sur une entreprise pour voir ses détails</p>
+    </div>
+
+
+</div>
