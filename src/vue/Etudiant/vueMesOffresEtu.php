eab83b12886edf1967be671c1abcaaa0e380e783
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-27 22:20:41 +0100

    feat : refonte vueMesOffres -> début des travaux

--- src/vue/Etudiant/vueMesOffresEtu.php
+++ src/vue/Etudiant/vueMesOffresEtu.php
@@ -3,5 +3,17 @@ $etudiant = (new \App\FormatIUT\Modele\Repository\EtudiantRepository())->getObje
 ?>
 
 <div class="mainMesOffres">
-<p>t</p>
+
+    <div class="estCandidat">
+        <h2 class="titre">Mes offres postulées</h2>
+    </div>
+
+    <div class="enAttente">
+        <h2 class="titre">Mes offres validées</h2>
+    </div>
+
+    <div class="presMesOffres">
+
+    </div>
+
 </div>
