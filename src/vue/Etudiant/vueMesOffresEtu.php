365369320fe8ec2b4e74c348ae1964b25a983d00
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-12-28 17:05:20 +0100

    feat : vueMesOffres : ajout compteurs

--- src/vue/Etudiant/vueMesOffresEtu.php
+++ src/vue/Etudiant/vueMesOffresEtu.php
@@ -1,9 +1,28 @@
 <?php
 
 use App\FormatIUT\Modele\Repository\FormationRepository;
+use App\FormatIUT\Modele\Repository\PostulerRepository;
 
 $etudiant = (new \App\FormatIUT\Modele\Repository\EtudiantRepository())->getObjectParClePrimaire(\App\FormatIUT\Lib\ConnexionUtilisateur::getNumEtudiantConnecte());
 $listeOffres = (new FormationRepository())->listeOffresEtu($etudiant->getNumEtudiant());
+
+$listeOffresEnAttente = [];
+
+foreach ($listeOffres as $offre) {
+    if ((new PostulerRepository())->getEtatEtudiantOffre($etudiant->getNumEtudiant(), $offre->getIdFormation()) == "En attente") {
+        $listeOffresEnAttente[] = $offre;
+    }
+}
+
+$listeOffresAChoisirEtValidees = [];
+
+foreach ($listeOffres as $offre) {
+    if ((new PostulerRepository())->getEtatEtudiantOffre($etudiant->getNumEtudiant(), $offre->getIdFormation()) == "A Choisir" || (new PostulerRepository())->getEtatEtudiantOffre($etudiant->getNumEtudiant(), $offre->getIdFormation()) == "Validée") {
+        $listeOffresAChoisirEtValidees[] = $offre;
+    }
+}
+
+
 ?>
 
 <div class="mainMesOffres">
@@ -13,7 +32,7 @@ $listeOffres = (new FormationRepository())->listeOffresEtu($etudiant->getNumEtud
         <div class="front">
             <h2 class="titre">Mes offres postulées</h2>
             <div class="circle">
-                <span class="number">1</span>
+                <span class="number"><?php echo count($listeOffresEnAttente)?></span>
             </div>
         </div>
 
@@ -28,10 +47,9 @@ $listeOffres = (new FormationRepository())->listeOffresEtu($etudiant->getNumEtud
         <div class="front">
             <h2 class="titre">Mes offres validées</h2>
             <div class="circle">
-                <span class="number">1</span>
+                <span class="number"><?php echo count($listeOffresAChoisirEtValidees)?></span>
             </div>
         </div>
-
 
 
     </div>
