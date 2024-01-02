586daece243a098f87e9ada4c3469e2cdb6d647c
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-01-02 23:06:11 +0100

    feat : vue recherche : avancées

--- src/vue/vueResultatRecherche.php
+++ src/vue/vueResultatRecherche.php
@@ -7,4 +7,27 @@ use App\FormatIUT\Modele\Repository\FormationRepository;
 
 <div class="mainRecherche">
 
+    <div class="bodyRecherche">
+
+        <div class="controleRech">
+            <h2 class="titre" id="rouge">Effectuez une recherche sur Format'IUT</h2>
+            <?php
+            echo $codeRecherche
+            ?>
+            <div class="filtresRech">
+
+            </div>
+
+            <div class="trierPar">
+                <h4 class="titre">Trier Par :</h4>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="parametresRecherche">
+        <p></p>
+    </div>
+
 </div>
