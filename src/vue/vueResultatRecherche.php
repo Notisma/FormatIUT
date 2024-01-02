8254b55bf836da42e626bfb27bb9887652542c2b
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-01-02 23:18:50 +0100

    feat : vue recherche : avancées

--- src/vue/vueResultatRecherche.php
+++ src/vue/vueResultatRecherche.php
@@ -20,6 +20,24 @@ use App\FormatIUT\Modele\Repository\FormationRepository;
 
             <div class="trierPar">
                 <h4 class="titre">Trier Par :</h4>
+
+                <?php
+                //on récupère le contenu de l'url après le querry string
+                $url = $_SERVER['REQUEST_URI'];
+                $url = explode("?", $url);
+                $url = $url[1];
+                $url = "?" . $url;
+                //echo $url;
+                ?>
+
+                <form method="get" action="<?php echo $url ?>" id="formTrierPar">
+                    <select name="triPar" onchange="this.form.submit()">
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                        <!-- Ajoutez autant d'options que nécessaire -->
+                    </select>
+                </form>
             </div>
 
         </div>
