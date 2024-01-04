520657608bef4f94f44a141cb9f10496e98e18f4
TouzeR <romain.touze@etu.umontpellier.fr>
2024-01-05 00:11:10 +0100

    feature : privilège filtre recherche avec label Personnels.php

--- src/Lib/Users/Personnels.php
+++ src/Lib/Users/Personnels.php
@@ -35,22 +35,22 @@ class Personnels extends Utilisateur
     public function getFiltresRecherche(): array
     {
         return array(
-            "filtre1"=>"entreprise_validee",
-            "filtre2"=>"entreprise_non_validee",
-            "filtre3"=>"etudiant_A1",
-            "filtre4"=>"etudiant_A2",
-            "filtre5"=>"etudiant_A3",
-            "filtre6"=>"etudiant_avec_formation",
-            "filtre7"=>"etudiant_sans_formation",
-            "filtre8"=>"etudiant_stage",
-            "filtre9"=>"etudiant_alternance",
-            "filtre10"=>"formation_stage",
-            "filtre11"=>"formation_alternance",
-            "filtre12"=>"formation_validee",
-            "filtre13"=>"formation_non_validee",
-            "filtre14"=>"personnel_prof",
-            "filtre15"=>"personnel_admin",
-            "filtre16"=>"personnel_secretariat"
+            "filtre1"=>array("label"=>"Entreprises Validées","value"=>"entreprise_validee"),
+            "filtre2"=>array("label"=>"Entreprises Non Validées","value"=>"entreprise_non_validee"),
+            "filtre3"=>array("label"=>"Etudiants A1","value"=>"etudiant_A1"),
+            "filtre4"=>array("label"=>"Etudiants A2","value"=>"etudiant_A2"),
+            "filtre5"=>array("label"=>"Etudiants A3","value"=>"etudiant_A3"),
+            "filtre6"=>array("label"=>"Etudiants Avec Formations","value"=>"etudiant_avec_formation"),
+            "filtre7"=>array("label"=>"Etudiants Sans Formations","value"=>"etudiant_sans_formation"),
+            "filtre8"=>array("label"=>"Stagiaires","value"=>"etudiant_stage"),
+            "filtre9"=>array("label"=>"Alternants","value"=>"etudiant_alternance"),
+            "filtre10"=>array("label"=>"Stages","value"=>"formation_stage"),
+            "filtre11"=>array("label"=>"Alternances","value"=>"formation_alternance"),
+            "filtre12"=>array("label"=>"Formations Validées","value"=>"formation_validee"),
+            "filtre13"=>array("label"=>"Formations Non Validées","value"=>"formation_non_validee"),
+            "filtre14"=>array("label"=>"Profs","value"=>"personnel_prof"),
+            "filtre15"=>array("label"=>"Administrateurs","value"=>"personnel_admin"),
+            "filtre16"=>array("label"=>"Secretariat","value"=>"personnel_secretariat")
         );
     }
 }
