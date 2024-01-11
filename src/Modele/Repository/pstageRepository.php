39479cb572aded1ab577aa36b028ab339aa42ff1
Tordeux Matteo <matteo.tordeux@etu.umontpellier.fr>
2024-01-11 18:42:54 +0100

    bugfix : correction erreur requête SQL exportCSV

--- src/Modele/Repository/pstageRepository.php
+++ src/Modele/Repository/pstageRepository.php
@@ -216,7 +216,13 @@ class pstageRepository extends AbstractRepository
     public function exportCSV(): array
     {
         $sql = "SELECT etu.numEtudiant, prenomEtudiant, nomEtudiant, etu.mailUniversitaire, groupe, parcours,offr.validationPedagogique, typeOffre, dateCreationConvention ,dateDebut, dateFin, nomEntreprise, mailTuteurPro, nomProf 
-        FROM Etudiants etu LEFT JOIN Postuler r ON r.numEtudiant = etu.numEtudiant LEFT JOIN Formations offr ON offr.idFormation = r.idFormation LEFT JOIN Entreprises entr ON entr.numSiret = offr.idEntreprise LEFT JOIN Villes vEntr ON vEntr.idVille = entr.idVille LEFT JOIN TuteursPro tp ON offr.idTuteurPro = tp.idTuteurPro LEFT JOIN Profs prof ON prof.loginProf= offr.loginTuteurUM";
+FROM Etudiants etu LEFT JOIN Postuler r ON r.numEtudiant = etu.numEtudiant 
+LEFT JOIN Formations offr ON offr.idFormation = r.idFormation 
+LEFT JOIN Entreprises entr ON entr.numSiret = offr.idEntreprise 
+LEFT JOIN Villes vEntr ON vEntr.idVille = entr.idVille 
+LEFT JOIN TuteursPro tp ON offr.idTuteurPro = tp.idTuteurPro 
+LEFT JOIN Profs prof ON prof.loginProf= offr.loginTuteurUM
+WHERE etat = 'Validée'";
         $pdoStatement = ConnexionBaseDeDonnee::getPdo()->query($sql);
         $pdoStatement->setFetchMode(\PDO::FETCH_NUM);
         $listeObjet = array();
