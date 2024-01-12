92567e87e613f138a31177b2e05bdc2f88d957d3
Noé Fuertes <noe.fuertes-torredeme@etu.umontpellier.fr>
2024-01-12 13:31:32 +0100

    feat: mise en page de la vue

--- src/vue/Admin/vueStatistiques.php
+++ src/vue/Admin/vueStatistiques.php
@@ -1,3 +1,31 @@
-<div>
-
+<div class="statistiques">
+    <div class="grille">
+        <div id="etu" class="gridElem">
+            <h4 class="titreStats">Statistiques sur les étudiants</h4>
+            <div>
+                <p>Nombre d'étudiants inscrits sur Format'IUT: placeholder</p>
+                <p>Nombre d'étudiants ayant postulé à une offre: placeholder</p>
+                <p>Nombre d'étudiants ayant une formation: placeholder</p>
+            </div>
+        </div>
+        <div id="entr" class="gridElem">
+            <h4 class="titreStats">Statistiques sur les entreprises</h4>
+            <div>
+                <p>Nombre d'entreprises inscrites sur Format'IUT: placeholder</p>
+                <p>Nombre d'entreprises n'ayant pas soumis d'offre: placeholder</p>
+                <p>Nombre moyen d'offres par entreprise: placeholder</p>
+            </div>
+        </div>
+        <div id="form" class="gridElem">
+            <h4 class="titreStats">Statistiques sur les formations</h4>
+            <div>
+                <p>Nombre d'offres de formation sur le site: placeholder</p>
+                <p>Nombre d'offres de stage: placeholder</p>
+                <p>Nombre d'offres d'alternance: placeholder</p>
+                <p>Nombre d'offres non validées: placeholder</p>
+                <p>Nombre de formations assignées à un étudiant: placeholder </p>
+                <p>Nombre de formations n'ayant pas de convention validée: placeholder</p>
+            </div>
+        </div>
+    </div>
 </div>
