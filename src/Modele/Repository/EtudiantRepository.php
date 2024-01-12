f73c1fa0940450be038180878d4353803219f418
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-01-12 23:11:01 +0100

    feat : avancées

--- src/Modele/Repository/EtudiantRepository.php
+++ src/Modele/Repository/EtudiantRepository.php
@@ -147,7 +147,7 @@ class EtudiantRepository extends RechercheRepository
      */
     public function etudiantsEnAttente($idFormation): array
     {
-        $sql = "SELECT numEtudiant FROM Postuler r WHERE idFormation=:Tag AND etat!='Annulé' AND NOT EXISTS(SELECT * FROM Formations f WHERE r.numEtudiant=f.idEtudiant)";
+        $sql = "SELECT numEtudiant FROM Postuler r WHERE idFormation=:Tag AND etat!='Annulé'";
         $pdoStatement = ConnexionBaseDeDonnee::getPdo()->prepare($sql);
         $values = array("Tag" => $idFormation);
         $pdoStatement->execute($values);
