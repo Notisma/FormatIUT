a9b10636abbe32f5a83a85308f26429d0d0192ba
Noé Fuertes <noe.fuertes-torredeme@etu.umontpellier.fr>
2024-01-12 15:16:17 +0100

    feat: amélioration de la vue

--- src/vue/Admin/vueStatistiques.php
+++ src/vue/Admin/vueStatistiques.php
@@ -2,29 +2,29 @@
     <div class="grille">
         <div id="etu" class="gridElem">
             <h4 class="titreStats">Statistiques sur les étudiants</h4>
-            <div>
-                <p>Nombre d'étudiants inscrits sur Format'IUT: placeholder</p>
-                <p>Nombre d'étudiants ayant postulé à une offre: placeholder</p>
-                <p>Nombre d'étudiants ayant une formation: placeholder</p>
+            <div class="listeStats">
+                <p class="pStat">Nombre d'étudiants inscrits sur Format'IUT: placeholder</p>
+                <p class="pStat">Nombre d'étudiants ayant postulé à une offre: placeholder</p>
+                <p class="pStat">Nombre d'étudiants ayant une formation: placeholder</p>
             </div>
         </div>
         <div id="entr" class="gridElem">
             <h4 class="titreStats">Statistiques sur les entreprises</h4>
-            <div>
-                <p>Nombre d'entreprises inscrites sur Format'IUT: placeholder</p>
-                <p>Nombre d'entreprises n'ayant pas soumis d'offre: placeholder</p>
-                <p>Nombre moyen d'offres par entreprise: placeholder</p>
+            <div class="listeStats">
+                <p class="pStat">Nombre d'entreprises inscrites sur Format'IUT: placeholder</p>
+                <p class="pStat">Nombre d'entreprises n'ayant pas soumis d'offre: placeholder</p>
+                <p class="pStat">Nombre moyen d'offres par entreprise: placeholder</p>
             </div>
         </div>
         <div id="form" class="gridElem">
             <h4 class="titreStats">Statistiques sur les formations</h4>
-            <div>
-                <p>Nombre d'offres de formation sur le site: placeholder</p>
-                <p>Nombre d'offres de stage: placeholder</p>
-                <p>Nombre d'offres d'alternance: placeholder</p>
-                <p>Nombre d'offres non validées: placeholder</p>
-                <p>Nombre de formations assignées à un étudiant: placeholder </p>
-                <p>Nombre de formations n'ayant pas de convention validée: placeholder</p>
+            <div class="listeStats" id="listeForm">
+                <p class="pStat">Nombre d'offres de formation sur le site: placeholder</p>
+                <p class="pStat">Nombre d'offres de stage: placeholder</p>
+                <p class="pStat">Nombre d'offres d'alternance: placeholder</p>
+                <p class="pStat">Nombre d'offres non validées: placeholder</p>
+                <p class="pStat">Nombre de formations assignées à un étudiant: placeholder </p>
+                <p class="pStat">Nombre de formations n'ayant pas de convention validée: placeholder</p>
             </div>
         </div>
     </div>
