b742afc983f21fd806897f8948a62cc19208df7f
Jean Hubert <izoret.raphael@gmail.com>
2024-01-13 15:07:59 +0100

    refactor :enlevé des vieux repo nuls

--- src/Modele/Repository/ConventionAlternanceRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\FormatIUT\Modele\Repository;
-
-class ConventionAlternanceRepository
-{
-
-}
--- src/Modele/Repository/ConventionStageRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\FormatIUT\Modele\Repository;
-
-class ConventionStageRepository extends ConventionRepository{
-
-}
