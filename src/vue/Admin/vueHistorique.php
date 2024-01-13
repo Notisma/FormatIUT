f479b664c8b4a1c60d60e5ae97bf6ee9e20aad98
fuertesn <noe.fuertes-torredeme@etu.umontpellier.fr>
2024-01-13 12:58:29 +0100

    feat: mise en page de la vue historique

--- src/vue/Admin/vueHistorique.php
+++ src/vue/Admin/vueHistorique.php
@@ -1,3 +1,32 @@
-<div class="historique">
-
+<div class="historiqueMain">
+    <div class="stats">
+        <div class="elem">
+            <h4 class="titreStats">Statistiques des étudiants sur les dernières années</h4>
+            <div>
+                <p>Nombre d'étudiants inscrits l'année dernière : placeholder</p>
+                <p>Nombre d'étudiants inscrits en moyenne chaque année : placeholder</p>
+                <p>Nombre d'étudiants ayant eu une formation l'année dernière : placeholder</p>
+                <p>Nombre d'étudiants ayant eu une formation en moyenne chaque année : placeholder</p>
+            </div>
+        </div>
+        <div class="elem">
+            <h4 class="titreStats">Statistiques des entreprises sur les dernières années</h4>
+            <div>
+                <p>Nombre d'entreprises étant déjà sur Format'IUT l'année dernière : placeholder</p>
+                <p>Nombre moyen d'entreprises présentant des offres chaque année : placeholder</p>
+                <p>Nombre d'entreprises n'ayant pas soumis d'offre l'année dernière : placeholder</p>
+            </div>
+        </div>
+        <div class="elem">
+            <h4 class="titreStats">Statistiques sur les formations les dernières années</h4>
+            <div>
+                <p>Nombre d'offres de formation postées en moyenne chaque année : placeholder</p>
+                <p>Nombre d'offres de formation postées l'année dernière : placeholder</p>
+                <p>Nombre de formations n'ayant pas eu de convention validée l'année dernière : placeholder</p>
+            </div>
+        </div>
+    </div>
+    <div class="boutonStats">
+        <a href="?action=afficherVueStatistiques"><button>Voir statistiques principales</button></a>
+    </div>
 </div>
