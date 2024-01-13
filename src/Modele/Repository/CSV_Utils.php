f9a113a3c0e284867edfb411575cac0d3bbfb189
Jean Hubert <izoret.raphael@gmail.com>
2024-01-13 15:20:08 +0100

    refactor : rename csv utils

--- src/Modele/Repository/CSV_Utils.php
+++ src/Modele/Repository/CSV_Utils.php
@@ -2,19 +2,19 @@
 
 namespace App\FormatIUT\Modele\Repository;
 
-use App\FormatIUT\Modele\DataObject\pstage;
-
-class pstageRepository {
+class CSV_Utils
+{
     public function exportCSV(): array
     {
         $sql = "SELECT etu.numEtudiant, prenomEtudiant, nomEtudiant, etu.mailUniversitaire, groupe, parcours,offr.validationPedagogique, typeOffre, dateCreationConvention ,dateDebut, dateFin, nomEntreprise, mailTuteurPro, nomProf 
-FROM Etudiants etu LEFT JOIN Postuler r ON r.numEtudiant = etu.numEtudiant 
-LEFT JOIN Formations offr ON offr.idFormation = r.idFormation 
-LEFT JOIN Entreprises entr ON entr.numSiret = offr.idEntreprise 
-LEFT JOIN Villes vEntr ON vEntr.idVille = entr.idVille 
-LEFT JOIN TuteursPro tp ON offr.idTuteurPro = tp.idTuteurPro 
-LEFT JOIN Profs prof ON prof.loginProf= offr.loginTuteurUM
-WHERE etat = 'Validée'";
+                FROM Etudiants etu LEFT JOIN Postuler r ON r.numEtudiant = etu.numEtudiant 
+                LEFT JOIN Formations offr ON offr.idFormation = r.idFormation 
+                LEFT JOIN Entreprises entr ON entr.numSiret = offr.idEntreprise 
+                LEFT JOIN Villes vEntr ON vEntr.idVille = entr.idVille 
+                LEFT JOIN TuteursPro tp ON offr.idTuteurPro = tp.idTuteurPro 
+                LEFT JOIN Profs prof ON prof.loginProf= offr.loginTuteurUM
+                WHERE etat = 'Validée'
+        ";
         $pdoStatement = ConnexionBaseDeDonnee::getPdo()->query($sql);
         $pdoStatement->setFetchMode(\PDO::FETCH_NUM);
         $listeObjet = array();
