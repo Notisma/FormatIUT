b04a083e61f85f0e92220d5e20d0480119708024
TouzeR <romain.touze@etu.umontpellier.fr>
2024-01-13 01:57:11 +0100

    feat : amélioration recherche + filtres triple 'ou'

--- src/Lib/Recherche/FiltresSQL/FiltresEntreprise.php
+++ src/Lib/Recherche/FiltresSQL/FiltresEntreprise.php
@@ -4,22 +4,22 @@ namespace App\FormatIUT\Lib\Recherche\FiltresSQL;
 
 class FiltresEntreprise
 {
-    public static function entreprise_validee():?string
+    public static function entreprise_validee(): string
     {
-        if (self::validite_entreprise())
-        return " estValide=1 ";
-        else return null;
+        $sql = " estValide=1 ";
+        return self::validite_entreprise($sql);
     }
-    public static function entreprise_non_validee():?string
+
+    public static function entreprise_non_validee(): string
     {
-        if (self::validite_entreprise())
-        return " estValide=0";
-        else return null;
+        $sql= "estValide=0";
+        return self::validite_entreprise($sql);
     }
-    public  static function validite_entreprise():bool
+
+    public static function validite_entreprise(string $sql): string
     {
-     if (isset($_REQUEST["entreprise_validee"],$_REQUEST["entreprise_non_validee"])){
-         return false;
-     }   else return true;
+        if (isset($_REQUEST["entreprise_validee"], $_REQUEST["entreprise_non_validee"])) {
+            return "";
+        } else return $sql;
     }
 }
--- src/Lib/Recherche/FiltresSQL/FiltresProf.php
+++ src/Lib/Recherche/FiltresSQL/FiltresProf.php
@@ -6,14 +6,50 @@ class FiltresProf
 {
     public static function personnel_prof():string
     {
-        return " (estAdmin=0 AND loginProf LIKE \"%ecretariat%\")";
+        $sql= " (estAdmin=0 AND loginProf NOT LIKE \"%ecretariat%\")";
+        return self::type_prof($sql);
     }
     public static function personnel_admin():string
     {
-        return " estAdmin=1";
+        $sql= " estAdmin=1";
+        return self::type_prof($sql);
+
     }
     public static function personnel_secretariat():string
     {
-        return " loginProf LIKE \"%ecretariat%\" ";
+        $sql= " loginProf LIKE \"%ecretariat%\" ";
+        return self::type_prof($sql);
+
+    }
+    public static function type_prof(string $filtre): string
+    {
+        $filtreL = array();
+        foreach ($_REQUEST as $item) {
+            if (in_array($item, get_class_methods("App\\FormatIUT\\Lib\\Recherche\\FiltresSQL\\FiltresProf"))) {
+                if (str_contains($item, "personnel_")) {
+                    $filtreL[] = $item;
+                }
+            }
+        }
+        if (count($filtreL) > 1) {
+            $sql="(";
+            foreach ($filtreL as $item) {
+                if (str_contains($item, "prof")) {
+                    $sql.="(estAdmin=0 AND loginProf NOT LIKE \"%ecretariat%\")";
+                } else if (str_contains($item, "admin")) {
+                    $sql.=" estAdmin=1 ";
+                } else if (str_contains($item, "secretariat")) {
+                    $sql.="loginProf LIKE \"%ecretariat%\" ";
+                }
+                if ($item !=$filtreL[count($filtreL)-1]){
+                    $sql.=" OR ";
+                }
+            }
+            $sql.=")";
+            return $sql;
+        } else {
+            return $filtre;
+        }
+
     }
 }
