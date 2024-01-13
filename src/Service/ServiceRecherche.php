f2c937d03335411ebbdb53fe456bcc43c84a1dc0
TouzeR <romain.touze@etu.umontpellier.fr>
2024-01-13 12:38:43 +0100

    docs : crash de recherche

--- src/Service/ServiceRecherche.php
+++ src/Service/ServiceRecherche.php
@@ -20,73 +20,70 @@ class ServiceRecherche
      */
     public static function rechercher(): void
     {
-        self::verifRecherche();
-
-        $recherche = $_REQUEST['recherche'];
-        $morceaux = explode(" ", $recherche);
-
-        $res = AbstractRepository::getResultatRechercheTrie($morceaux);
-        $liste = array();
-        $count = 0;
-        $privilege=ConnexionUtilisateur::getUtilisateurConnecte()->getFiltresRecherche();
-        $sansfiltres=true;
-        foreach ($privilege as $item=>$value) {
-            if (isset($_REQUEST[$item."s"])){$sansfiltres=false;}
-        }
-        foreach ($privilege as $repository=>$filtres) {
-            $listeFiltres= self::filtresFunction($repository,$filtres);
-            if (isset($_REQUEST[$repository.'s']) || $sansfiltres) {
-                $nomDeClasseRepository = "App\FormatIUT\Modele\Repository\\" . $repository . "Repository";
-                $re = "recherche";
-                $nomAffichageRecherche = "App\FormatIUT\Lib\Recherche\AffichagesRecherche\\" . $repository . "Recherche";
-                $element = (new $nomDeClasseRepository)->$re($morceaux,$listeFiltres);
-                $arrayRecherche = array();
-                foreach ($element as $objet) {
-                    $arrayRecherche[] = new $nomAffichageRecherche($objet);
+        if (self::verifRecherche()) {
+            $recherche = $_REQUEST['recherche'];
+            $morceaux = explode(" ", $recherche);
+            $liste = array();
+            $count = 0;
+            $privilege = ConnexionUtilisateur::getUtilisateurConnecte()->getFiltresRecherche();
+            $sansfiltres = true;
+            foreach ($privilege as $item => $value) {
+                if (isset($_REQUEST[$item . "s"])) {
+                    $sansfiltres = false;
                 }
-                $liste[$repository] = $arrayRecherche;
-                $count += sizeof($liste[$repository]);
             }
-        }
+            foreach ($privilege as $repository => $filtres) {
+                $listeFiltres = self::filtresFunction($repository, $filtres);
+                if (isset($_REQUEST[$repository . 's']) || $sansfiltres) {
+                    $nomDeClasseRepository = "App\FormatIUT\Modele\Repository\\" . $repository . "Repository";
+                    $re = "recherche";
+                    $nomAffichageRecherche = "App\FormatIUT\Lib\Recherche\AffichagesRecherche\\" . $repository . "Recherche";
+                    $element = (new $nomDeClasseRepository)->$re($morceaux, $listeFiltres);
+                    $arrayRecherche = array();
+                    foreach ($element as $objet) {
+                        $arrayRecherche[] = new $nomAffichageRecherche($objet);
+                    }
+                    $liste[$repository] = $arrayRecherche;
+                    $count += sizeof($liste[$repository]);
+                }
+            }
 
-        if (is_null($res)) { // jamais censé être null, même en cas de zéro résultat
-            MessageFlash::ajouter("danger", "Crash de recherche");
-            die();
-        } else {
             MessageFlash::ajouter("success", "$count résultats trouvés.");
             $_REQUEST["recherche"] = $recherche;
             $_REQUEST["liste"] = $liste;
             $_REQUEST["count"] = $count;
             ControleurMain::afficherRecherche();
+        } else {
+            var_dump("de");
+            header("Location: $_SERVER[HTTP_REFERER]");;
         }
     }
 
-    private static function filtresFunction(string $repository,array $filtres)
+    private static function filtresFunction(string $repository, array $filtres)
     {
-        $listeFiltres=array();
+        $listeFiltres = array();
         foreach ($filtres as $filtre) {
-            if (in_array("obligatoire",$filtre) ||isset($_REQUEST[$filtre["value"]])){
-                $fonction=$filtre["value"];
-                $nomDeClasseFiltre="App\FormatIUT\Lib\Recherche\FiltresSQL\Filtres".$repository;
-                $listeFiltres[]=$nomDeClasseFiltre::$fonction();
+            if (in_array("obligatoire", $filtre) || isset($_REQUEST[$filtre["value"]])) {
+                $fonction = $filtre["value"];
+                $nomDeClasseFiltre = "App\FormatIUT\Lib\Recherche\FiltresSQL\Filtres" . $repository;
+                $listeFiltres[] = $nomDeClasseFiltre::$fonction();
             }
         }
         return $listeFiltres;
     }
 
-    private static function verifRecherche()
+    private static function verifRecherche(): bool
     {
         if (!isset($_REQUEST['recherche'])) {
             MessageFlash::ajouter("warning", "Veuillez renseigner une recherche.");
-            header("Location: $_SERVER[HTTP_REFERER]");
-            return;
+            return false;
         }
         ////si la recherche ne contient que un ou des espaces
         if (preg_match('/^\s+$/', $_REQUEST['recherche'])) {
             MessageFlash::ajouter("warning", "Veuillez renseigner une recherche valide.");
-            header("Location: $_SERVER[HTTP_REFERER]");
-            return;
+            return false;
         }
+        return true;
     }
 
 }
